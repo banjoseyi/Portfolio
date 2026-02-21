17093f34e90175b0a4e9481179b2f521728a4d85
banjo oluwaseyifunmi <banjoseyi2@gmail.com>
2026-02-21 20:40:11 +0100

    Updated the portfolio

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -96,10 +96,19 @@ export default function Homepage() {
                             download
                             className="about-btn holographic-btn"
                         >
-                            Download Resume
+                            Download Resume <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                                <polyline points="7 10 12 15 17 10" />
+                                <line x1="12" y1="15" x2="12" y2="3" />
+                            </svg>
                         </a>
                         <Link to="/resume" className="about-btn active holographic-btn">
                             Go to Resume page
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+                                <polyline points="15 3 21 3 21 9" />
+                                <line x1="10" y1="14" x2="21" y2="3" />
+                            </svg>
                         </Link>
                     </div>
                 </div>
